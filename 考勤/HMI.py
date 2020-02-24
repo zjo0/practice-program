b759b09da5b1911e10c1a676943d704e763680cf
zjo0 <37432498+zjo0@users.noreply.github.com>
2020-02-24 19:30:38 +0800

    规范天数

--- 考勤/HMI.py
+++ 考勤/HMI.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import filedialog
 from 考勤 import unionrecord
+import datetime
 
 class Application(Frame):
     def __init__(self,master=None):
@@ -9,18 +10,29 @@ class Application(Frame):
         self.officepath=StringVar()   
         self.dingdingpath=StringVar()
         self.workoffpath=StringVar()
+        self.year_=IntVar()
+        yearget=datetime.datetime.now().year
+        self.year_.set(yearget)
+        self.month_=IntVar()
+        monthget=datetime.datetime.now().month
+        self.month_.set(monthget)
         self.createWidgets()
+
     def createWidgets(self):
         self.officeLabel=Label(self,text='办公室打卡文件路径').grid(row=0,column=0)
         self.entry=Entry(self, textvariable=self.officepath).grid(row=0, column=1)
-        self.selectButton1=Button(self,text='路径选择',command=self.select_office_path).grid(row=0,column=2)
+        self.selectButton1=Button(self,text='路径选择',command=self.select_office_path).grid(row=0,column=3)
         self.dingdingLabel = Label(self, text='钉钉打卡文件路径').grid(row=1, column=0)
         self.entry = Entry(self, textvariable=self.dingdingpath).grid(row=1, column=1)
-        self.selectButton2 = Button(self, text='路径选择', command=self.select_dingding_path).grid(row=1, column=2)
+        self.selectButton2 = Button(self, text='路径选择', command=self.select_dingding_path).grid(row=1, column=3)
         self.workoffLabel = Label(self, text='请假文件路径').grid(row=2, column=0)
         self.entry = Entry(self, textvariable=self.workoffpath).grid(row=2, column=1)
-        self.selectButton3 = Button(self, text='路径选择', command=self.select_workoff_path).grid(row=2, column=2)
-        self.createButton=Button(self,text='生成汇总文件',command=lambda : unionrecord(self.officepath.get(),self.dingdingpath.get(),self.workoffpath.get())).grid(row=3)
+        self.selectButton3 = Button(self, text='路径选择', command=self.select_workoff_path).grid(row=2, column=3)
+        self.officeLabel=Label(self,text='考勤年份').grid(row=3,column=0)
+        self.entry=Entry(self, textvariable=self.year_).grid(row=3, column=1)
+        self.officeLabel=Label(self,text='考勤月份').grid(row=3,column=2)
+        self.entry=Entry(self, textvariable=self.month_).grid(row=3, column=3)
+        self.createButton=Button(self,text='生成汇总文件',command=lambda : unionrecord(self.officepath.get(),self.dingdingpath.get(),self.workoffpath.get(),self.year_.get(),self.month_.get())).grid(row=4)
     def select_office_path(self):
         path1_=filedialog.askopenfilename()
         path1_=path1_.replace('/','\\\\')
--- 考勤/考勤.py
+++ 考勤/考勤.py
@@ -1,16 +1,26 @@
 import numpy as np
 import pandas as pd
-def unionrecord(officepath, dingdingpath, dayoffrecordpath):
-    office_original = pd.read_excel(officepath)
-    office_original['日期'] = pd.to_datetime(office_original['日期'])  # convert type from str to datetime
-    dingding_original = pd.read_excel(dingdingpath, header=2)  # header=2 to ignore first two row
-    dingding_original['日期'] = pd.to_datetime(dingding_original['日期'])
-    office_record = office_original.set_index(['姓名', '日期']).loc[:, ['上班签到时间', '下班签退时间']].rename({'上班签到时间': '单位签到时间', '下班签退时间': '单位签退时间'}, axis='columns')  # set multiindex and rename them
-    dingding_sort = dingding_original.set_index(['姓名', '日期']).rename({'aBian': '卞庆荣','李珺琪 yuki':'李珺琪'})
-    dingding_onrecord = dingding_sort.groupby(['姓名', '日期']).min().loc[:, ['地点', '时间']].rename({'地点': '钉钉上班地', '时间': '钉钉上班时间'}, axis='columns')  # select the last item for each day as the last record time
-    dingding_offrecord = dingding_sort.groupby(['姓名', '日期']).max().loc[:, ['地点', '时间']].rename({'地点': '钉钉下班地', '时间': '钉钉下班时间'}, axis='columns')  # select the first item for each day as the last record time
-    dayoffrecord_original = pd.read_excel(dayoffrecordpath, header=1)
-    dayoffrecord_ = [pd.DataFrame(columns=['姓名', '日期', '是否请假']).set_index('日期')]
+import calendar
+def unionrecord(officepath, dingdingpath, dayoffrecordpath,year_,month_):
+    wholedate=pd.date_range('%s-%s-01'%(year_,month_),periods=calendar.monthrange(year_,month_)[1])
+    try:
+        office_original = pd.read_excel(officepath)
+        office_original['日期'] = pd.to_datetime(office_original['日期'])  # convert type from str to datetime
+        office_record = office_original.set_index(['姓名', '日期']).loc[:, ['上班签到时间', '下班签退时间']].rename({'上班签到时间': '单位签到时间', '下班签退时间': '单位签退时间'}, axis='columns')  # set multiindex and rename them
+    except:
+        print('办公室打卡记录提取失败')
+        office_record=pd.DataFrame(columns=['姓名','日期','单位签到时间','单位签退时间']).set_index(['姓名','日期'])#读取办公室打卡记录失败后，建立空文件
+    try:
+        dingding_original = pd.read_excel(dingdingpath, header=2)  # header=2 to ignore first two row
+        dingding_original['日期'] = pd.to_datetime(dingding_original['日期'])
+        dingding_sort = dingding_original.set_index(['姓名', '日期']).rename({'aBian': '卞庆荣','李珺琪 yuki':'李珺琪'})
+        dingding_onrecord = dingding_sort.groupby(['姓名', '日期']).min().loc[:, ['地点', '时间']].rename({'地点': '钉钉上班地', '时间': '钉钉上班时间'}, axis='columns')  # select the last item for each day as the last record time
+        dingding_offrecord = dingding_sort.groupby(['姓名', '日期']).max().loc[:, ['地点', '时间']].rename({'地点': '钉钉下班地', '时间': '钉钉下班时间'}, axis='columns')  # select the first item for each day as the last record time
+    except:
+        print('钉钉打卡记录读取失败')
+        dingding_onrecord = pd.DataFrame(columns=['姓名','日期','钉钉上班地','钉钉上班时间']).set_index(['姓名','日期'])#读取钉钉打卡记录失败后，建立空文件
+        dingding_offrecord = pd.DataFrame(columns=['姓名','日期','钉钉下班地','钉钉下班时间']).set_index(['姓名','日期'])#读取钉钉打卡记录失败后，建立空文件
+    
 
     def huizong(a,dayoffrecord_):
         b = pd.DataFrame(index=pd.Index(pd.date_range(a['开始时间'], a['结束时间'], freq='D'), name='日期'))
@@ -18,11 +28,19 @@ def unionrecord(officepath, dingdingpath, dayoffrecordpath):
         b['是否请假'] = True
         dayoffrecord_[0] = dayoffrecord_[0].append(b)
 
-    dayoffrecord_original.apply(lambda x:huizong(x,dayoffrecord_),axis=1)  # work off start date and end date are listed in the same row in the original data
-    dayoffrecord = dayoffrecord_[0]
-    dayoffrecord = dayoffrecord.set_index(['姓名', dayoffrecord.index]).sort_values(['姓名', '日期'])
+    try:
+        dayoffrecord_original = pd.read_excel(dayoffrecordpath, header=1)
+        dayoffrecord_ = [pd.DataFrame(columns=['姓名', '日期', '是否请假']).set_index('日期')]
+        dayoffrecord_original.apply(lambda x:huizong(x,dayoffrecord_),axis=1)  # work off start date and end date are listed in the same row in the original data
+        dayoffrecord = dayoffrecord_[0]
+        dayoffrecord = dayoffrecord.set_index(['姓名', dayoffrecord.index]).sort_values(['姓名', '日期'])
+    except:
+        print('请假记录读取失败')
+        dayoffrecord =pd.DataFrame(columns=['姓名','日期','是否请假']).set_index(['姓名','日期'])#读取请假记录失败后，建立空文件
     union_record = office_record.copy()
     union_record = union_record.reindex(union_record.index.union(dayoffrecord.index).union(dingding_onrecord.index).union(dingding_offrecord.index))  # three DataFrame index united
+    wholeindex=pd.MultiIndex.from_product([union_record.index.get_level_values(0).drop_duplicates(),wholedate],names=['姓名','日期'])#读取三个文件中提到的所有姓名，与本月所有的日期组成MultiIndex
+    union_record=union_record.reindex(union_record.index.union(wholeindex))
     union_record = union_record.join(dingding_onrecord).join(dingding_offrecord).join(dayoffrecord)  # three DataFrame united
     union_record['是否正常'] = '异常'
 
@@ -50,11 +68,11 @@ def unionrecord(officepath, dingdingpath, dayoffrecordpath):
         if (a['是否请假'] == True):
             a['是否正常'] = '正常'
         if (a1==3):
-            a['是否正常']= '凌晨有打卡'
+            a['是否正常']='凌晨有打卡'
         return a
 
     union_record.apply(detect, axis=1)  # determine whether one person record has issue for each day
-    union_record.loc[union_record.index.get_level_values(1).weekday > 4, '是否正常'] = '周末'
+    union_record.loc[union_record.index.get_level_values(1).weekday>4,'是否正常'] ='周末'
     union_record.to_excel('union.xlsx')
     print('work done')
 
